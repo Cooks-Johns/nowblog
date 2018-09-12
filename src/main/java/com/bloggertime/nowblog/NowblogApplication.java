a240f81a5d52182bbc8c8358fe0865864dff8534
John <cooks.johns@gmail.com>
2018-09-12 16:06:08 -0500

    Initial commit

---
+++ src/main/java/com/bloggertime/nowblog/NowblogApplication.java
@@ -0,0 +1,12 @@
+package com.bloggertime.nowblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NowblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NowblogApplication.class, args);
+	}
+}
